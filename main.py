d7dbffb28504d19428cdd7f3d05faf83b0ce16b8
IlyaChef <ilyaromanov89@gmail.com>
2023-02-24 21:37:28 +0300

    functions splitted

--- main.py
+++ main.py
@@ -19,13 +19,21 @@ def parse_weather_data(data: dict) -> dict:
     }
 
 
-def get_weather(city: str, weather_api_key: str) -> dict:
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&units=metric&lang=en"
+def get_weather_url(city: str, weather_api_key: str) -> str:
+    return f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&units=metric&lang=en"
+
+
+def get_weather_data(url: str) -> dict:
     response = requests.get(url)
     if response.status_code != 200:
         error_message = response.json().get('message', 'City not found')
         raise HTTPException(status_code=response.status_code, detail=error_message)
-    data = response.json()
+    return response.json()
+
+
+def get_weather(city: str, weather_api_key: str) -> dict:
+    url = get_weather_url(city, weather_api_key)
+    data = get_weather_data(url)
     return parse_weather_data(data)
 
 

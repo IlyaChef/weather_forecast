0b6e5b1cd04882acd22af55b6f5a543adb46030f
IlyaChef <ilyaromanov89@gmail.com>
2023-02-24 21:00:55 +0300

    function splitted

--- main.py
+++ main.py
@@ -8,20 +8,25 @@ load_dotenv()
 
 app = FastAPI()
 
-WEATHER_API_KEY = getenv("WEATHER_API_KEY")
+weather_api_key = getenv("WEATHER_API_KEY")
 
 
-def get_weather(city: str) -> dict:
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric&lang=en"
-    response = requests.get(url)
-    data = response.json()
+def parse_weather_data(data: dict) -> dict:
     return {
         "temperature": data["main"]["temp"],
         "feels_like": data["main"]["feels_like"],
         "description": data["weather"][0]["description"],
     }
 
+
+def get_weather(city: str, weather_api_key: str) -> dict:
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&units=metric&lang=en"
+    response = requests.get(url)
+    data = response.json()
+    return parse_weather_data(data)
+
+
 @app.get("/weather/{city_name}")
 def weather(city_name: str):
-    weather = get_weather(city_name)
+    weather = get_weather(city_name, weather_api_key)
     return weather

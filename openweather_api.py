8a9705d70caf565ba553600d54736b2903531bad
IlyaChef <ilyaromanov89@gmail.com>
2023-02-28 13:29:13 +0300

    params added

--- openweather_api.py
+++ openweather_api.py
@@ -9,12 +9,19 @@ load_dotenv()
 
 weather_api_key = getenv("WEATHER_API_KEY")
 
-def get_weather_url(city: str, weather_api_key: str) -> str:
-    return f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&units=metric&lang=en"
+
+def get_weather_url(city: str, weather_api_key: str) -> tuple[str, dict[str, str]]:
+    params = {
+        "q": city,
+        "appid": weather_api_key,
+        "units": "metric",
+        "lang": "en"
+    }
+    return "https://api.openweathermap.org/data/2.5/weather", params
 
 
-def get_weather_data(url: str) -> dict:
-    response = requests.get(url)
+def get_weather_data(url: str, params: dict = None) -> dict:
+    response = requests.get(url, params=params)
     if response.status_code != 200:
         error_message = response.json().get('message', 'City not found')
         raise HTTPException(status_code=response.status_code, detail=error_message)
@@ -22,6 +29,6 @@ def get_weather_data(url: str) -> dict:
 
 
 def get_weather(city: str, weather_api_key: str) -> dict:
-    url = get_weather_url(city, weather_api_key)
-    data = get_weather_data(url)
+    url, params = get_weather_url(city, weather_api_key)
+    data = get_weather_data(url, params=params)
     return parse_weather_data(data)

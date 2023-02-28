2466907762da0a40fedbc24a8e31fc785091e0f6
IlyaChef <ilyaromanov89@gmail.com>
2023-02-28 10:37:42 +0300

    weather app split into modules

--- main.py
+++ main.py
@@ -1,40 +1,9 @@
-import requests
-from fastapi import FastAPI, HTTPException
-from dotenv import load_dotenv
-from os import getenv
+from fastapi import FastAPI
+from openweather_api import get_weather
+from openweather_api import weather_api_key
 
-
-load_dotenv()
 
 app = FastAPI()
-
-weather_api_key = getenv("WEATHER_API_KEY")
-
-
-def parse_weather_data(data: dict) -> dict:
-    return {
-        "temperature": data["main"]["temp"],
-        "feels_like": data["main"]["feels_like"],
-        "description": data["weather"][0]["description"],
-    }
-
-
-def get_weather_url(city: str, weather_api_key: str) -> str:
-    return f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&units=metric&lang=en"
-
-
-def get_weather_data(url: str) -> dict:
-    response = requests.get(url)
-    if response.status_code != 200:
-        error_message = response.json().get('message', 'City not found')
-        raise HTTPException(status_code=response.status_code, detail=error_message)
-    return response.json()
-
-
-def get_weather(city: str, weather_api_key: str) -> dict:
-    url = get_weather_url(city, weather_api_key)
-    data = get_weather_data(url)
-    return parse_weather_data(data)
 
 
 @app.get("/weather/{city_name}")
